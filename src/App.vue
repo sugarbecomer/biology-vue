5e1625ce9f16bf377bba1f03a927c0939bdde7f3
becomer <me@becomer.cn>
2024-01-16 14:04:55 +0800

    完成layout布局

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,113 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <d-layout class="my-layout">
+    <d-header class="dheader-2">
+      <img class="logo-devui" src="./assets/vue.svg" alt="" />
+      <span class="text">生信系统</span>
+    </d-header>
+    <d-layout>
+      <d-aside class="daside-2">
+        <d-accordion :data="menu" class="menu"></d-accordion>
+      </d-aside>
+      <d-content class="dcontent-2">
+        <d-breadcrumb class="dbreadcrumb">
+          <d-breadcrumb-item>
+            <span>生信系统</span>
+          </d-breadcrumb-item>
+          <d-breadcrumb-item>
+            <span>面包屑</span>
+          </d-breadcrumb-item>
+        </d-breadcrumb>
+        <div class="inner-content"></div>
+      </d-content>
+    </d-layout>
+    <d-footer class="dfooter-2">版权所有</d-footer>
+  </d-layout>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const menu = ref([
+      {
+        title: '品系',
+        open: false,
+      },
+      {
+        title: '基因',
+        open: false,
+      },
+      {
+        title: '注解',
+      },
+    ])
+    return {
+      menu
+    }
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+:deep(.devui-layout__header){
+  flex: unset;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.my-layout {
+  height: 100vh;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.daside-2 {
+  border-left: 1px solid transparent;
+  .menu{
+    width: 180px;
+  }
 }
+
+.dheader-2 {
+  position: relative;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  background-color: #333854;
+
+  .logo-devui {
+    width: 26px;
+    height: 26px;
+    margin: 0 10px;
+  }
+
+  .text {
+    color: #fff;
+    font-size: 16px;
+  }
+}
+
+.dcontent-2 {
+  height: 100%;
+  padding: 0px 40px 40px 40px;
+  background-color: #f3f6f8;
+
+  .inner-content {
+    background-color: #fff;
+    padding: 16px;
+    height: calc(100% - 20px);
+  }
+
+  .dbreadcrumb {
+    margin-top: 8px;
+    margin-bottom: 10px;
+  }
+}
+
+.dfooter-2 {
+  height: 50px;
+  color: #fff;
+  background-color: #333854;
+  padding: 8px 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import App from "./App.vue";
+import DevUI from 'vue-devui';
+import './style.scss'
+import 'vue-devui/style.css';
+import '@devui-design/icons/icomoon/devui-icon.css';
+import { ThemeServiceInit, infinityTheme, provenceTheme } from 'devui-theme';
 
-createApp(App).mount('#app')
+const themeService = ThemeServiceInit({ infinityTheme }, 'infinityTheme');
+themeService?.applyTheme(provenceTheme)
+
+const app = createApp(App)
+app.use(DevUI)
+app.mount('#app')
